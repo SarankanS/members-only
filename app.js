8d42a859fb944156e0333381e0df51fa58f2b8ca
Sarankan Srikaran <sarankansrikaran@gmail.com>
2025-07-31 09:27:29 -0400

    Initial commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the Members only!");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
