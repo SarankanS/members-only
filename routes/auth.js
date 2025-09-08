b3b47a8722db5682825e2506d50988092688a580
Sarankan Srikaran <sarankansrikaran@gmail.com>
2025-09-08 15:39:36 -0400

    Add input validation for signup

--- routes/auth.js
+++ routes/auth.js
@@ -1,3 +1,4 @@
+const {body} = require("express-validator")
 const { Router } = require('express');
 const authController = require('../controllers/authController');
 
@@ -7,7 +8,34 @@ authRouter.get("/login", authController.renderLoginForm);
 authRouter.post("/login", authController.login);
 
 authRouter.get("/signup", authController.renderSignUpForm);
-authRouter.post("/signup", authController.signup);
+
+authRouter.post("/signup",[
+    body('fname')
+      .trim()
+      .notEmpty().withMessage('Username is required')
+      .isLength({ min: 3 }).withMessage('Username must be at least 3 characters long'),
+
+    body('lname')
+    .trim()
+    .notEmpty().withMessage('Username is required')
+    .isLength({ min: 3 }).withMessage('Username must be at least 3 characters long'),
+
+
+    body('email')
+      .isEmail().withMessage('Invalid email')
+      .normalizeEmail(),
+
+    body('password')
+      .isLength({ min: 6 }).withMessage('Password must be at least 6 characters long'),
+
+    body('cpassword')
+      .custom((value, { req }) => {
+        if (value !== req.body.password) {
+          throw new Error("Passwords do not match");
+        }
+        return true;
+      })
+  ], authController.signup);
 
 authRouter.get("/logout", authController.logout);
 
