7be05ee0645686fa48406325b7a71291a6e3e0bc
Sarankan Srikaran <sarankansrikaran@gmail.com>
2025-09-09 13:15:55 -0400

    Implement logout with error handling; add join club feature with passcode validation and membership update

--- controllers/authController.js
+++ controllers/authController.js
@@ -38,14 +38,49 @@ async function signup(req,res){
 
 
 async function logout(req,res){
-    return null;
+    req.logout(function(err) {
+        if (err) {
+            console.error("Logout error:", err);
+            return next(err);
+        }
+        res.redirect('/');
+    });
 }
 
+async function renderJoinForm(req,res) {
+    if (!req.user){
+        return es.redirect('/auth/login');
+    }
+    res.render('join', { title: 'Join the Club', error: null });
+}
+
+async function joinClub(req,res){
+    if (!req.user){
+        return res.redirect("/auth/login");
+    }
+    const { passcode } = req.body;
+
+    if (passcode === process.env.SECRET_CLUB_CODE){
+        try{
+            db.updateMembership(req.user.id, true)
+            res.redirect("/")
+
+        }catch(err){
+            console.error("Join club error:", err);
+            res.status(500).send("An error occurred while updating membership.");
+        }
+    }else {
+        res.render('join', { title: 'Join the Club', error: 'Incorrect passcode!' });
+    }
+
+}
 
 module.exports = {
     renderLoginForm,
     login,
     renderSignUpForm,
     signup,
-    logout
+    logout,
+    renderJoinForm,
+    joinClub
 }
