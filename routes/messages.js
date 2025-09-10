1bbedd7f4bb61bc3faec5bcc2215aa7165d327b5
Sarankan Srikaran <sarankansrikaran@gmail.com>
2025-09-10 10:00:36 -0400

    Protect message routes with authentication and connect to message controller

--- routes/messages.js
+++ routes/messages.js
@@ -1,6 +1,10 @@
 const { Router } = require('express');
-const authController = require('../controllers/authController');
-
 const messageRouter = Router();
+const messageController = require('../controllers/messageController');
+const { ensureAuthenticated } = require('../middleware/auth'); 
+
+messageRouter.get("/new", ensureAuthenticated, messageController.renderMessageForm)
+messageRouter.post("/new", ensureAuthenticated, messageController.createMessage);
+
 
 module.exports = messageRouter;
