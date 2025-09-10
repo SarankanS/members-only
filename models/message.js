11c2915a01b31251e587b981335fc7a7741997d1
Sarankan Srikaran <sarankansrikaran@gmail.com>
2025-09-10 09:56:21 -0400

    Enhance message model with pagination, author info, and message count queries

--- models/message.js
+++ models/message.js
@@ -1,9 +1,8 @@
-const { use } = require("passport");
-const pool = require("./pool");
+const pool = require("../db/pool");
 
 async function createMessage(title,content, userId) {
     return await pool.query(
-        `INSERT INTO messages (title,content,userID)
+        `INSERT INTO messages (title,content,author_ID)
         VALUES ($1,$2,$3)`, [title,content,userId]
     )    
 }
@@ -15,7 +14,35 @@ async function getMessageByUser(userID){
     return result.rows;
 }
 
+async function getAllMessages() {
+    const results = await pool.query(
+        `SELECT messages.id, messages.title, messages.content, messages.timestamp, users.firstname || ' ' || users.lastname AS authorname
+        FROM messages
+        JOIN users ON messages.author_id = users.id
+        ORDER BY messages.timestamp DESC`
+    );
+
+    return results.rows;   
+}
+
+async function getMessages(limit, offset) {
+  const result = await pool.query(
+    'SELECT * FROM messages ORDER BY timestamp DESC LIMIT $1 OFFSET $2',
+    [limit, offset]
+  );
+  return result.rows;
+}
+
+async function getMessageCount() {
+  const result = await pool.query('SELECT COUNT(*) FROM messages');
+  return parseInt(result.rows[0].count);
+}
+
+
 module.exports = {
     createMessage,
-    getMessageByUser
+    getMessageByUser,
+    getAllMessages,
+    getMessages,
+    getMessageCount
 }
